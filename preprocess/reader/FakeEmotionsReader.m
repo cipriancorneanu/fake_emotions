f62b63a91515508f998e4bcb429c19109fc7bf46
CiprianCorneanu <cipriancorneanu@gmail.com>
2017-01-19 15:18:04 +0100

    Processing changed to accomodate new input.

--- preprocess/reader/FakeEmotionsReader.m
+++ preprocess/reader/FakeEmotionsReader.m
@@ -13,32 +13,67 @@ classdef FakeEmotionsReader < DataReader
             % Get all subdirectories in path; each one corresponds to a
             % different person
             PersonSubdirs = GetSubdirs(obj, obj.DataPath);
-            images = [];
-            landmarks = [];
-            emotions = {};
+            
+            %% Load geometry detector
+            model = importdata('../models/model_sdm_cofw.mat');
             
             for person = 1:length(PersonSubdirs) 
                 fprintf(strcat('Reading person:\t', PersonSubdirs{person}, '\n'));                              
                 Path = strcat(obj.DataPath, PersonSubdirs{person}, filesep);
-                             
-                % Get all video files from each subdir. each one corrsponds
-                % to a different emotion
-                rgb_files = GetFiles(obj, Path, '*.MP4');              
+                images = [];
+                landmarks = [];
+                emotions = {};
+                visualize = false;
+            
+                % Get all subdirs in path; each one corresponds to a
+                % different label
+                LabelSubdirs = GetSubdirs(obj, Path);
+                
+                for label = 1:length(LabelSubdirs)
+                    fprintf(strcat('Reading emotion:\t', LabelSubdirs{person}, '\n'));  
+                    
+                    % Get all files from each subdir. Each one corresponds
+                    % to a different frame
+                    Path = strcat(Path, LabelSubdirs{label}, filesep);
+                    rgb_files = GetFiles(obj, Path, '*.png');   
+                    
+                    % Sort files
+                    tokens = cellfun(@(x) strsplit(x,'['), rgb_files, 'UniformOutput', false);
+                    tokens = cellfun(@(x) x(1), tokens);
+                    [tokens, idx] = sort(cellfun(@(x) str2num(x), tokens));
+                    rgb_files = rgb_files(:,idx);
+                                    
+                    % Init containers
+                    numFrames = length(rgb_files);
+                    ims = zeros(numFrames,224,224,3);
+                    lms = zeros(numFrames,29,2);
+                    emos = cell(numFrames,1);
 
-                for i = 1:length(rgb_files)
-                    fprintf(strcat('Reading video:\t', rgb_files{i}, '\n'));  
-                    if numel(rgb_files{i})>1
-                        [ims, lms, emos] = proc_video(Path, rgb_files{i});
-                    else
-                        fprintf('\t\t No files found\n');
+                    for i = 1:30%length(rgb_files)
+                        frame = imread(strcat(Path, rgb_files{i}));
+
+                        % Fit shape
+                        [geom, ~] = fitFrame(frame, [1 1 224 224], model);
+
+                        ims(i,:,:,:) = frame;        
+                        lms(i,:,:) = geom;
+                        emos{i} = LabelSubdirs{label};
+                        
+                        if visualize
+                           imshow(frame);
+                           hold on;
+                           scatter(geom(:,1), geom(:,2), 'r.');
+                           pause(0.5);
+                        end
                     end
+                    
                     images = [images; ims];
                     landmarks = [landmarks; lms];
                     emotions = [emotions; emos];
                 end
                 
                 % Write to file
-                out = {'ims', 'lms', 'emos'; images, landmarks, emotions};
+                out = {images, landmarks, emotions};
                 save(strcat(obj.SavePath, num2str(person), '.mat'), 'out', '-v7.3');
             end
         end
--- preprocess/reader/read.m
+++ preprocess/reader/read.m
@@ -1,6 +1,6 @@
 % Main reading script for BU4DFE
 
-path2data = '/Users/cipriancorneanu/Research/data/fake_emotions/FakeEmotion_Videos/';
+path2data = '/Users/cipriancorneanu/Research/data/fake_emotions_proc/';
 path2save = '/Users/cipriancorneanu/Research/data/fake_emotions_proc/';
 
 % Add dependecies
