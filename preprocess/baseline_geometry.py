4f5cc2c726e655ebc354c24cad8d9f2542531a59
CiprianCorneanu <cipriancorneanu@gmail.com>
2017-01-25 09:34:37 +0100

    Script added for formatting fnames

--- preprocess/baseline_geometry.py
+++ preprocess/baseline_geometry.py
@@ -1,16 +1,23 @@
 __author__ = 'cipriancorneanu'
 
 from reader.loader import load_fake
-from processor.partitioner import *
+from processor.partitioner import slice
 import processor.encoder as enc
-from cascaded.toolkit import procrustes, linalg
+import os
 import numpy as np
+import cPickle
+from fastdtw import fastdtw
+from scipy.spatial.distance import euclidean
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 
+def load_baseline(path, fname):
+    if os.path.exists(path+fname):
+        return cPickle.load(open(path+fname, 'rb'))
 
-if __name__ == '__main__':
     # Load data
     path2data = '/Users/cipriancorneanu/Research/data/fake_emotions/geoms/'
-    data = np.asarray(load_fake(path2data, 'femo.pkl'))
+    data = np.asarray(load_fake(path, 'femo.pkl'))
 
     # Split in the two classes
     true = np.concatenate([x[:6] for x in data])
@@ -25,4 +32,38 @@ if __name__ == '__main__':
     # Encode (procustes + pca)
     coded, mean, T = enc.encode_parametric(uncoded)
 
-    pass
+    # Keep non-rigid params only
+    coded = coded[:,:4]
+
+    # Split back
+    data = [coded[us] for us in uncoded_slices]
+    target = np.asarray(
+        np.concatenate([np.ones((len(uncoded_slices)/2,1)), 0*np.ones((len(uncoded_slices)/2,1))]),
+        dtype=np.int
+    )
+
+    cPickle.dump((data, target), open(path+fname, 'wb'), cPickle.HIGHEST_PROTOCOL)
+
+    return (data, target)
+
+def split_data(data, target, split=0.2):
+    return train_test_split(data, target, test_size=split, random_state=0)
+
+if __name__ == '__main__':
+    # Load data
+    path = '/Users/cipriancorneanu/Research/data/fake_emotions/geoms/'
+    (data, target) = load_baseline(path, 'femo_baseline.pkl')
+
+    # Split into train test
+    X_train, X_test, y_train, y_test = split_data(data, target)
+
+    # Classify
+    res = []
+    for i,te in enumerate(X_test):
+        print 'Classifying test sample ' + str(i)
+        res.append([fastdtw(te, tr, dist=euclidean) for tr in X_train])
+
+    cPickle.dump(res, open(path+'results.pkl', 'wb'), cPickle.HIGHEST_PROTOCOL)
+
+
+
--- scripts/change_fnames.py
+++ scripts/change_fnames.py
@@ -2,5 +2,22 @@ __author__ = 'cipriancorneanu'
 
 import os
 
-[os.rename(f, 'frame'+f) for f in os.listdir('.') if f.endsiwith('.png')]
+path = '/data/hupba2/Datasets/FakefaceDataProc/Extracted_faces'
+
+for dir in os.listdir(path):
+    for emo in os.listdir(os.path.join(path,dir)):
+        for f in os.listdir(os.path.join(path,dir,emo)):
+            if f.endswith('.png'):
+                if f.startswith('frame'):
+                    root = f.split('.')[0][5:]
+                elif '[' in f:
+                    root = f.split('[')[0]
+                else:
+                    root = f.split('.')
+
+                ext = '0'*(5 - len(root))
+
+                print os.path.join(path,dir,emo,ext+root)
+
+                #os.rename(f, ext+root)
 
