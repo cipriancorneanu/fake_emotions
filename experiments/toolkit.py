79ac7e1a7b0948644d486e116b2f530f72ce70f1
CiprianCorneanu <cipriancorneanu@gmail.com>
2017-01-27 19:45:11 +0100

    Sift features loader added.

--- experiments/toolkit.py
+++ experiments/toolkit.py
@@ -6,8 +6,44 @@ import scipy.io
 import numpy as np
 
 def read_femo_sift(path):
-    femo_sift = None
-    return femo_sift
+    n_persons, n_classes = (54, 12)
+    data = [[None for _ in range(n_classes)] for _ in range(n_persons)]
+
+    person_keys = ['_'+str(x)+'_' for x in np.arange(1,n_persons)]
+    target_keys = ['act_HAPPY', 'act_SAD', 'act_CONTEMPT', 'act_SUPRISED', 'act_DISGUST', 'act_ANGRY',
+                   'fake_HAPPY', 'fake_SAD', 'fake_CONTEMPT', 'fake_SUPRISED', 'fake_DISGUST', 'fake_ANGRY']
+
+    mapping = {'act':{'HAPPY':0, 'SAD':1, 'CONTEMPT':2, 'SUPRISED':3, 'DISGUST':4, 'ANGRY':5},
+              'fake':{'HAPPY':6, 'SAD':7, 'CONTEMPT':8, 'SUPRISED':9, 'DISGUST':10, 'ANGRY':11}}
+
+    files = [f for f in os.listdir(path)]
+
+    # Slice by sequence
+    for p_key in person_keys:
+        person_seq = [f for f in files if p_key in f]
+
+        #Slice by target
+        for t_key in target_keys:
+            print 'person:{} target:{}'.format(p_key[1:3],t_key)
+
+            target_seq = [f for f in person_seq if t_key in f]
+            seq = [None]*len(target_seq)
+
+            # Read sequence
+            for i,f in enumerate(target_seq):
+                # Parse fname
+                tokens = f.split('.')[0].split('_')
+                category, fe, person, frame = (tokens[2], tokens[3], int(tokens[1]), int(tokens[0][3:])-1)
+                target = mapping[category][fe]
+
+                # Load data
+                fdata = cPickle.load(open(path+f, 'rb'))
+                seq[frame] = np.asarray(fdata, dtype=np.int16)
+
+            data[person][target] = seq
+
+    cPickle.dump(data, open(path+'femo_sift.pkl', 'wb'), cPickle.HIGHEST_PROTOCOL)
+    return data
 
 def pkl2mat(path, fname):
     (X,y) = cPickle.load(open(path+fname+'.pkl', 'rb'))
@@ -27,6 +63,8 @@ def leave_one_out_femo():
     return (train, test)
 
 if __name__ == '__main__':
-    path = '/Users/cipriancorneanu/Research/data/fake_emotions/geoms/'
+    path = '/Users/cipriancorneanu/Research/data/fake_emotions/sift/'
 
-    pkl2mat(path, 'femo_geom_proc')
+    read_femo_sift(path)
+
+
