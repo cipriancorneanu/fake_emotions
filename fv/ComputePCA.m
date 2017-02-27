4a71bbdcedf16f9578781a9565be7b9943d6647f
CiprianCorneanu <cipriancorneanu@gmail.com>
2017-02-27 17:27:22 +0100

    Updates to FV.

--- fv/ComputePCA.m
+++ fv/ComputePCA.m
@@ -2,7 +2,6 @@ function [reducedfeats, mean_col,U,S,V]  = ComputePCA(feats,dim)
 
 %feat = cell2mat(feat);
 
-
 mean_col = mean(feats);
 
 data_cent = feats-repmat(mean_col,size(feats,1),1);
@@ -10,7 +9,6 @@ data_cent = feats-repmat(mean_col,size(feats,1),1);
 covData = data_cent'*data_cent;
 
 [U S V] = svd(covData);
-
 
 reducedfeats = feats*U(:,1:dim);
 
--- fv/TrajFisherMainRun.m
+++ fv/TrajFisherMainRun.m
@@ -6,21 +6,25 @@ addpath(genpath('../../vlfeat-0.9.20'));
 vl_setup;
 
 addpath('/home/kulkarni/softs/libsvm-3.22/matlab');
+savepath('/home/corneanu/data/fake_emotions/idt/gmm/full/')
 
-trajPath = '~/Research/data/fake_emotions/improved_dense_trajectories/fake_faces/';
+trajPath = '/home/corneanu/data/fake_emotions/idt/';
 corr_recog = 0;
 total_eg = 0;
 confusion = zeros(12,12);   
+boost = 20;
     
-[hog, hof, mbh, slices, lbls] = load_idt('/Users/cipriancorneanu/Research/data/fake_emotions/improved_dense_trajectories/');
-descriptor = {hog, hof, mbh};
+[hog, hof, mbh, slices, lbls] = load_idt(trajPath);
+descriptor = {hog};
+
+res = zeros(length(descriptor), length(descriptor{1}));
 
 for d = 1:length(descriptor)
-    fprintf('FV with descriptor %d\n', d);
+    fprintf('-------->FV with descriptor %d\n', d);
     descr = descriptor{d};    
     folds = 1:length(descr);
     
-    for i = 1:length(descriptor)   
+    for i = 1:length(descr)   
         %% Define train and test
         folds_test = i;
         folds_train = setdiff(folds, folds_test);
@@ -37,86 +41,83 @@ for d = 1:length(descriptor)
         slices_test = slices{folds_test};
         labels_test = lbls{folds_test};
 
-        for numCenters = [64]
+        numCenters = [256, 512];
+        for n = 1:len(numCenters)
             %% Compute GMMs for from training data
-            fprintf('Computing GMMs fold %d centers %d\n',i,numCenters);
+            fprintf('Computing GMMs fold %d centers %d\n',i,numCenters(n));
             train_data = cat(1,train{:});
-            sampled_train_data = train_data(randsample(size(train_data, 1), size(train_data, 1)/10), :);
-            [mu,sigma,w] = ComputeKmeans(sampled_train_data,numCenters);
+	    
+            sampled_train_data = train_data(randsample(size(train_data, 1), int64(size(train_data, 1)/10)), :);
+
+            [mu,sigma,w] = ComputeKmeans(sampled_train_data,numCenters(n));
+            save([savepath,'gmm_', d, '_', i, '_', n, '.mat'], 'mu', 'sigma', 'w');
                  
             %% Compute FV per sequences train
             fishervecstrain = []; lbltrain=[]; fishervecstest=[];lbltest=[];
             for s = 1:length(slices_train)
                 person_slice = slices_train{s};
-                
+
+                % Slice sequence
                 for seq = 1:size(person_slice,1)
-                    % Slice sequence
                     sequence = train{s}(person_slice(seq,1):person_slice(seq,2), :);
-                    size(sequence)
                     Vec = vl_fisher(sequence',mu,sigma,w,'Improved');
                     fishervecstrain = [fishervecstrain,Vec];
-                    lbltrain = [lbltrain; labels_train{s}(seq)];
+                    lbltrain = [lbltrain; labels_train{s}(seq)];                    
+                end               
+                
+                %{
+                for seq = 1:size(person_slice,1)
+                len_slice = (person_slice(seq,2) - person_slice(seq,1));
+                    len = int64(0.55*len_slice); 
+                    starts = randsample(int64(0.4*len_slice), boost);
+
+                    for st = 1:length(starts)
+                        sequence = train{s}(person_slice(seq,1)+starts(st): ...
+                        person_slice(seq,1)+starts(st)+len, :);
+                        Vec = vl_fisher(sequence',mu,sigma,w,'Improved');
+                        fishervecstrain = [fishervecstrain,Vec];
+                        lbltrain = [lbltrain; labels_train{s}(seq)];
+                    end
                 end
+                %}
             end
             
             %% Compute FV per sequences test
             person_slice = slices_test;
             for seq = 1:size(person_slice,1)
                 % Slice sequence
-                sequence = test(person_slice(seq,1):person_slice(seq,2), :) ;
+                sequence = test(person_slice(seq,1):person_slice(seq,2), :);
                 Vec = vl_fisher(sequence',mu,sigma,w,'Improved');
-                fishervecstest = [fishervecstest,Vec];
+                fishervecstest = [fishervecstest,Vec];              
                 lbltest = [lbltest; labels_test(seq)];
             end        
 
-            %{
-            for j = 1 : no_of_folds
-                if i~=j
-                    for k = folds(j)+1:folds(j+1)
-                        act = uniqAct{k};
-                        ind = find(strcmp(actors,act));
-                        for l = 1:size(ind,2)
-                            filename = gunzip([trajPath,act,'_',instances{ind(l)},'.features.gz'],pwd);
-                            [feats,stats,points] = readtraj(filename{1});
-                            feats = feats(:,205:end);
-                            feats = (feats-repmat(mean_col,size(feats,1),1))*U(:,1:pcadim);
-                            Vec = vl_fisher(feats',mu,sigma,w,'Improved');
-                            fishervecstrain = [fishervecstrain,Vec];
-                            delete(filename{1});
-                            lbltrain = [lbltrain,labels(ind(l))];
-                        end;
-                    end;
-                else
-                    for k = folds(j)+1:folds(j+1)
-                        act = uniqAct{k};
-                        ind = find(strcmp(actors,act));
-                        for l = 1:size(ind,2)
-                            filename = gunzip([trajPath,act,'_',instances{ind(l)},'.features.gz'],pwd);
-                            [feats,stats,points] = readtraj(filename{1});
-                            feats = feats(:,205:end);
-                            feats = (feats-repmat(mean_col,size(feats,1),1))*U(:,1:pcadim);
-                            Vec = vl_fisher(feats',mu,sigma,w,'Improved');
-                            fishervecstest = [fishervecstest,Vec];
-                            delete(filename{1});
-                            lbltest = [lbltest,labels(ind(l))];
-                        end;
-                    end;
-                end;
-            end;
-            %}
+            %[fishervecstrain, mean,  U, S, V] = ComputePCA(fishervecstrain', 64);
+            %fishervecstest = (fishervecstest'-repmat(mean, size(fishervecstest',1),1))*U(:,1:64);
+            %size(fishervecstrain)
+            %size(fishervecstest)
 
-            fprintf('Training SVMs fold %d centers %d\n',i,numCenters);
-            faceModel = libsvmtrain(lbltrain',fishervecstrain','-s 0 -t 0 -c 100 -b 1 -q ');
+            fprintf('Training SVMs fold %d centers %d\n cost %d\n ',i,numCenters, c);
+            options = '-s 0 -t 0 -c 100 -b 1 -q ';
+            faceModel = libsvmtrain(double(lbltrain),double(fishervecstrain'), options);
             fprintf('Testing SVMs fold %d centers %d\n',i,numCenters);
-            [predicted_label, accuracy,prob_estimates] = libsvmpredict(lbltest', fishervecstest', faceModel, '-b 1');
+
+            [predicted_label, accuracy,prob_estimates] = libsvmpredict(double(lbltest), double(fishervecstest'), faceModel, '-b 1');
             %fprintf('accuracy=%f\n',accuracy);
+            
             for j = 1:size(predicted_label,1)
                 if predicted_label(j)== lbltest(j)
                     corr_recog = corr_recog+1;
                 end;
+                
                 total_eg = total_eg+1;
-                confusion(lbltest(j),predicted_label(j)) = confusion(lbltest(j),predicted_label(j)) +1;
+                confusion(lbltest(j)+1,predicted_label(j)+1) = confusion(lbltest(j)+1,predicted_label(j)+1) +1;
             end;
-        end;
-    end;
+            confusion
+            save([savepath,'conf_', num2str(d), '_', num2str(i), '_', num2str(n), '.mat'], 'confusion');
+            res(d,i,n) = accuracy(1);  
+        end; 
+   end;
 end;
+
+res
